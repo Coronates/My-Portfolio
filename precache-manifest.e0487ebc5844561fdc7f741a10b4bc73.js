2a3d9e42619883e7f96504e5c842b17ccdb1ab0b
Fabian Coronado <andres.coronado@endava.com>
2020-02-07 15:59:49 -0500

    Updates

---
+++ precache-manifest.e0487ebc5844561fdc7f741a10b4bc73.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c284d73a6d7ab8b3ef0459addda9c851",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9f01158071a47f8f7bf8",
+    "url": "/portfolio/static/css/2.bd72ddd2.chunk.css"
+  },
+  {
+    "revision": "e7a1a85460593e74044b",
+    "url": "/portfolio/static/css/main.53f11b01.chunk.css"
+  },
+  {
+    "revision": "9f01158071a47f8f7bf8",
+    "url": "/portfolio/static/js/2.fad5920b.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.fad5920b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7a1a85460593e74044b",
+    "url": "/portfolio/static/js/main.bb6cdc5d.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "585888bdebff1d5c7805be36ba8f1c19",
+    "url": "/portfolio/static/media/arm.585888bd.png"
+  },
+  {
+    "revision": "07f7436f1ec35b945e2656deca7ec401",
+    "url": "/portfolio/static/media/campus.07f7436f.png"
+  },
+  {
+    "revision": "f9a8a0597cdcef3f6b54cdd05e5fa770",
+    "url": "/portfolio/static/media/cartoon-done.f9a8a059.png"
+  },
+  {
+    "revision": "e0f7c5e14afa5d952e78e23f157cb9af",
+    "url": "/portfolio/static/media/cartoon.e0f7c5e1.png"
+  },
+  {
+    "revision": "5ad8a53754b934fca14781cf634929fb",
+    "url": "/portfolio/static/media/coding.5ad8a537.jpg"
+  },
+  {
+    "revision": "d3138a412fec9fbf451ff442190b78ac",
+    "url": "/portfolio/static/media/corfomento.d3138a41.png"
+  },
+  {
+    "revision": "1ac01f7b4614795c4ab41e50b823763b",
+    "url": "/portfolio/static/media/crimial-login.1ac01f7b.jpg"
+  },
+  {
+    "revision": "c54138a31b7199393b3071bf7e4d0143",
+    "url": "/portfolio/static/media/endava.c54138a3.png"
+  },
+  {
+    "revision": "15af774b61b0a991fc92536ac4156e0e",
+    "url": "/portfolio/static/media/location-icon.15af774b.svg"
+  },
+  {
+    "revision": "0992f9025f7519b83e1d4aa9a6b9c6c1",
+    "url": "/portfolio/static/media/pacific-battleship.0992f902.png"
+  },
+  {
+    "revision": "9833a3c7bd23f1ee2159e187d3eec430",
+    "url": "/portfolio/static/media/painting.9833a3c7.png"
+  },
+  {
+    "revision": "a73175fe7ce162583a02613fbc9b64bc",
+    "url": "/portfolio/static/media/sabana.a73175fe.png"
+  }
+]);
